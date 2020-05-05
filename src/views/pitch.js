1493c705e703a870b8d9617bbfd558a06132e802
Jenaro Calvino <jen.calvineo@gmail.com>
2020-05-05 16:50:45 -0300

    Added pitch page

--- src/views/pitch.js
+++ src/views/pitch.js
@@ -1,5 +1,39 @@
-import React from "react";
+//* @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import Title from "../components/Title";
+import Input from "../components/Input";
+import Button from "../components/Button";
 
 export default function Pitch() {
-  return <div></div>;
+  return (
+    <section
+      css={css`
+        background-color: var(--lavender);
+        color: var(--ember);
+        padding-top: 50px;
+        padding-bottom: 100px;
+      `}
+    >
+      <div className="container">
+        <Title color="var(--lavender)" borderColor="var(--ember)">
+          New Project.
+        </Title>
+        <p>
+          We are glad you decided to pitch your project to us, please fill in
+          the form below
+        </p>
+        <form>
+          <Input label="ong name:" name="ongName" id="ongName" />
+          <Input label="contact email:" name="contactEmail" id="contactEmail" />
+          <textarea
+            name="description"
+            id="description"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <Button>Submit Pitch</Button>
+        </form>
+      </div>
+    </section>
+  );
 }
