4ddcb7e9d0ded4f95a8caf0229d0480aa9008543
Jenaro Calvino <jen.calvineo@gmail.com>
2020-05-06 11:04:19 -0300

    Added projects and pitch pages

--- src/views/pitch.js
+++ src/views/pitch.js
@@ -12,6 +12,14 @@ export default function Pitch() {
         color: var(--ember);
         padding-top: 50px;
         padding-bottom: 100px;
+        min-height: calc(100vh - 288px);
+        border-top: 5px solid var(--ember);
+        border-bottom: 5px solid var(--ember);
+
+        @media (min-width: 420px) {
+          min-height: calc(100vh - 238px);
+          padding-bottom: 50px;
+        }
       `}
     >
       <div className="container">
@@ -23,14 +31,69 @@ export default function Pitch() {
           the form below
         </p>
         <form>
-          <Input label="ong name:" name="ongName" id="ongName" />
-          <Input label="contact email:" name="contactEmail" id="contactEmail" />
-          <textarea
-            name="description"
-            id="description"
-            cols="30"
-            rows="10"
-          ></textarea>
+          <Input
+            styles={css`
+              margin: 1rem 0;
+            `}
+            label="ong name:"
+            name="ongName"
+            id="ongName"
+          />
+          <Input
+            styles={css`
+              margin: 1rem 0;
+            `}
+            label="contact email:"
+            name="contactEmail"
+            id="contactEmail"
+          />
+          <label
+            htmlFor="description"
+            css={css`
+              font-size: 16px;
+              text-transform: uppercase;
+              width: 100%;
+              display: block;
+              position: relative;
+              margin-bottom: 1rem;
+
+              &::before {
+                content: "";
+                width: 5px;
+                height: 100%;
+                background-color: var(--ember);
+                position: absolute;
+                top: 0;
+                left: -15px;
+                transform: scaleX(0);
+                transform-origin: right;
+                transition: transform 0.2s ease-in-out;
+              }
+
+              &:focus-within {
+                &::before {
+                  transform: scaleX(1);
+                }
+              }
+            `}
+          >
+            Brief description of website:
+            <textarea
+              css={css`
+                padding: 16px;
+                width: calc(100% - 32px);
+                color: var(--lavender);
+                font-family: "Hind Madurai", sans-serif;
+                margin-top: 4px;
+                background-color: var(--ember);
+                border: 1px solid var(--lavender);
+              `}
+              name="description"
+              id="description"
+              cols="30"
+              rows="10"
+            ></textarea>
+          </label>
           <Button>Submit Pitch</Button>
         </form>
       </div>
--- src/views/projects.js
+++ src/views/projects.js
@@ -1,5 +1,35 @@
-import React from "react";
+//* @jsx jsx */
+import { css, jsx } from "@emotion/core";
+import ProjectItem from "../components/ProjectItem";
+import useProjects from "../hooks/use-projects";
 
 export default function Projects() {
-  return <div></div>;
+  const { data, loading, error } = useProjects();
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>Error!</p>;
+
+  const { projects } = data;
+
+  return (
+    <section
+      css={css`
+        padding-top: 50px;
+        padding-bottom: 100px;
+
+        @media (min-width: 768px) {
+          padding-bottom: 50px;
+        }
+      `}
+    >
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id}>
+            <ProjectItem project={project} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
