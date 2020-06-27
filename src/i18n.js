5791ccdcdc0453b4796f51b97973ba4baad98848
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2020-06-27 12:35:13 -0300

    Registered Spanish translation

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,14 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./translations/en.json";
+import es from "./translations/es.json";
 
 const resources = {
   en: {
     translation: en
+  },
+  es: {
+    translation: es
   }
 };
 
