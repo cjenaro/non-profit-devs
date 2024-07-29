0ec45321f6a19bea8b406eca9de93d2f3597e8a8
Jenaro Calviño <jen.calvineo@gmail.com>
2024-07-29 14:43:47 -0300

    create browser router

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,66 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import './i18n';
+import { UserProvider } from './context/UserContext.jsx';
+import Layout from './components/Layout.jsx';
+import Home from './views/home/index.jsx';
+import Profile from './views/profile';
+import Pitch from './views/pitch';
+import Projects from './views/projects';
+import Project from './views/project';
+import Login from './views/login';
+import Signup from './views/signup';
+import NotFound from './views/not-found';
 
 const root = createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+        loader: () => ({ message: 'HOME' }),
+      },
+      {
+        path: '/profile',
+        element: <Profile />,
+      },
+      {
+        path: '/pitch',
+        element: <Pitch />,
+      },
+      {
+        path: '/projects',
+        element: <Projects />,
+      },
+      {
+        path: '/projects/:id',
+        element: <Project />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/signup',
+        element: <Signup />,
+      },
+      {
+        path: '*',
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
   </React.StrictMode>
 );
 
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Hero from './hero.jsx';
 import WhoWeAre from './who-we-are.jsx';
 import HowItWorks from './how-it-works.jsx';
