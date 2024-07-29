231ce812b6f71b4936df4efe16bd5b35dceedc8c
Jenaro Calviño <jen.calvineo@gmail.com>
2024-07-29 14:23:13 -0300

    import issue

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
 //* @jsx jsx */
 import { jsx, css } from '@emotion/react';
-import styled from '@emotion/react';
+import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
 
 const StyledLink = styled(Link)`
