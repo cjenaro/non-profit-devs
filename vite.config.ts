a02f318dc44e6a7aec006043cb575ddac50c1a6a
Jenaro Calviño <jen.calvineo@gmail.com>
2024-07-29 15:04:31 -0300

    added tsx

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
+  build: {
+    sourcemap: true,
+  },
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
