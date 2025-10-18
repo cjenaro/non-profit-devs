06d4ca635763414e8346f1fa128f7c92e183f0cd
cjenaro <jen.calvineo@gmail.com>
2025-10-18 05:01:17 -0300

    swc

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   build: {
     sourcemap: true,
   },
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-    }),
-    // @ts-expect-error - for now
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
 });
