9928aa60b18497fa8c091c122130f21149e71813
cjenaro <jen.calvineo@gmail.com>
2025-10-29 20:38:10 -0300

    shadcn components

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
   base: '/',
@@ -8,4 +9,9 @@ export default defineConfig({
     sourcemap: true,
   },
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
